b641c154741fa056290a57f62855fc84340367ea
冯兵兵 <bingbingfeng2@yirendai.com>
2017-08-18 18:59:56 +0800

    clean code

--- spring-boot-mybatis3-multi-datasource/src/main/java/com/mindflow/springboot/mybatis/config/datasource/MasterDataSourceConfig.java
+++ spring-boot-mybatis3-multi-datasource/src/main/java/com/mindflow/springboot/mybatis/config/datasource/MasterDataSourceConfig.java
@@ -6,7 +6,6 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -15,10 +14,6 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import javax.sql.DataSource;
 
 /**
- * @Primary 标志这个 Bean 如果在多个同类 Bean 候选时，该 Bean 优先被考虑。「多数据源配置的时候注意，必须要有一个主数据源，用 @Primary 标志该 Bean」
- * @MapperScan 扫描 Mapper 接口并容器管理，包路径精确到 master，为了和下面 cluster 数据源做到精确区分
- * @Value 获取全局配置文件 application.properties 的 kv 配置,并自动装配
- * sqlSessionFactoryRef 表示定义了 key ，表示一个唯一 SqlSessionFactory 实例
  * @author Ricky Fung
  */
 @Configuration
