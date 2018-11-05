00a1830106b3d5e9a996dcbd93114f5e7f9dd053
Ricky Fung <vricky.feng@gmail.com>
2018-11-05 14:23:27 +0800

    Create BaseSpringBootJUnitTest.java

---
+++ spring-boot-mybatis/src/test/java/io/dreamstudio/springboot/mybatis/BaseSpringBootJUnitTest.java
@@ -0,0 +1,15 @@
+package io.dreamstudio.springboot.mybatis;
+
+import io.dreamstudio.mybatis.MybatisApp;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Ricky Fung
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MybatisApp.class)
+public class BaseSpringBootJUnitTest {
+
+}
