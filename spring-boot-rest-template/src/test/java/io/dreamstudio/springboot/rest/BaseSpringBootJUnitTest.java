1abb04e5a52380dc5d34dcb89b1dc041dbd02b63
Ricky Fung <vricky.feng@gmail.com>
2018-11-08 03:43:22 +0800

    第四篇: 整合Spring RestTemplate

--- spring-boot-rest-template/src/test/java/io/dreamstudio/springboot/rest/BaseSpringBootJUnitTest.java
+++ spring-boot-rest-template/src/test/java/io/dreamstudio/springboot/rest/BaseSpringBootJUnitTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class BaseSpringBootJUnitTest {
 
     @Test
-    public void testContext() {
+    public void testApp() {
 
     }
 }
