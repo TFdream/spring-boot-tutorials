5af73a5218a117551a680d2fc1c0f8a2033a00b6
Ricky Fung <vricky.feng@gmail.com>
2018-11-08 05:40:29 +0800

    第六篇: 整合阿里巴巴Druid数据库连接池

--- spring-boot-mybatis-druid/sql/1_init.sql
+++ spring-boot-mybatis-druid/sql/1_init.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE `user` (
 `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
 `name` varchar(45) NOT NULL COMMENT '用户名',
@@ -7,6 +6,5 @@ CREATE TABLE `user` (
 `version` int(10) NOT NULL DEFAULT '1' COMMENT '版本号',
 `create_time` datetime DEFAULT NULL COMMENT '创建时间',
 `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-PRIMARY KEY (`id`),
-UNIQUE KEY `uniq_user_id` (`user_id`,`activity_id`)
+PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
