67c55aaea206c565cf5f4bda04bc6b75375544ff
Ricky Fung <vricky.feng@gmail.com>
2018-11-08 05:39:42 +0800

    第二篇: 整合Mybatis

--- spring-boot-mybatis/sql/1_init.sql
+++ spring-boot-mybatis/sql/1_init.sql
@@ -7,6 +7,5 @@ CREATE TABLE `user` (
 `version` int(10) NOT NULL DEFAULT '1' COMMENT '版本号',
 `create_time` datetime DEFAULT NULL COMMENT '创建时间',
 `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-PRIMARY KEY (`id`),
-UNIQUE KEY `uniq_user_id` (`user_id`,`activity_id`)
+PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
